ba190e72739b4db4311f843eaa5c8909a287b0aa
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2020-03-04 12:36:33 -0800

    queue

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ stack.display();
 console.log("Adding Pikachu");
 stack.push("Pikachu");
 stack.display();
+
+var Queue = require('./queue');
+var queue = new Queue();
+queue.enqueue("Bird");
+queue.enqueue("Magic");
+queue.enqueue("Jordan");
+queue.enqueue("Kobe");
+queue.enqueue("Shaq");
+queue.display();
+console.log("You can stop waiting Bird");
+queue.dequeue();
+queue.display();
