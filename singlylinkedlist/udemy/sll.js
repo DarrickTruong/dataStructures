f632fbf2b55b600ff27c7766aa8e365e487aff39
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2020-07-27 17:36:25 -0700

    commit

--- singlylinkedlist/udemy/sll.js
+++ singlylinkedlist/udemy/sll.js
@@ -1,57 +1,82 @@
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
 class SLL {
-    constructor() {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return null;
+    } else {
+      if (this.head === this.tail) {
+        let popped = this.head;
         this.head = null;
         this.tail = null;
-        this.length = 0;
+        this.length--;
+        return popped;
+      }
+      var current = this.head;
+      while (current.next) {
+        var pre = current;
+        current = current.next;
+      }
+      pre.next = null;
+      this.tail = pre;
+      this.length--;
     }
+    return current;
+  }
 
-    push(val){
-        let newNode = new Node(val);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else { 
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    };
-
-    pop(){
-        if(!this.head){
-            return null
-        } else {
-            if (this.head === this.tail) {
-                let popped = this.head;
-                this.head = null;
-                this.tail = null;
-                this.length--;
-                return popped;
-            }
-            var current = this.head;
-            while(current.next) {
-                var pre = current;
-                current = current.next;
-            }
-            pre.next = null;
-            this.tail = pre;
-            this.length--;
-        }
-        return current;
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
     }
+    this.length++;
+    return this;
+  }
+
+  get(idx){
+      if(idx > this.length-1 || !this.head){
+          return null
+      }
+      let current = this.head;
+      while (idx>0){
+          current = current.next;
+          idx--;
+      }
+    return current.val
+  }
 }
 
 let sll = new SLL();
 
-console.log(sll.push(1))
+console.log(sll.push(1));
 console.log(sll.push(2));
 console.log(sll.push(3));
 
@@ -60,4 +85,12 @@ console.log(sll.pop());
 console.log(sll.pop());
 console.log(sll);
 
-
+console.log(sll.push(1));
+console.log(sll.push(2));
+console.log(sll.push(3));
+console.log(sll.unshift(4));
+console.log(sll.get(0));
+console.log(sll.get(1));
+console.log(sll.get(2));
+console.log(sll.get(3));
+console.log(sll.get(4));
