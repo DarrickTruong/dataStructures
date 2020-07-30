ed3750b80b800661f25b6883ea78c7ef3e2a9216
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2020-07-29 18:13:38 -0700

    doublylinked list

---
+++ doubleLinkedList/dll.js
@@ -0,0 +1,44 @@
+let Node = require("./node.js");
+
+class DoubleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.previous;
+      this.tail.next = null;
+      poppedNode.previous = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+}
+
+let dll = new DoubleLinkedList();
+
+console.log(dll.push(1));
+console.log(dll.push(2));
+console.log(dll.pop());
---
+++ doubleLinkedList/node.js
@@ -0,0 +1,11 @@
+let Node = (function() {
+    function Node(val) {
+        this.val = val;
+        this.head = null;
+        this.tail = null;
+    }
+    
+    return Node
+})();
+
+module.exports = Node; 
