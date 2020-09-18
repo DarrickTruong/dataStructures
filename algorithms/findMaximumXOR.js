7efdf5d6c47a5241e8308b1aa95e8925ffb5ecbb
Darrick Truong <truong.darrick@gmail.com>
2020-09-17 22:49:14 -0700

    isRobotBound

--- algorithms/findMaximumXOR.js
+++ algorithms/findMaximumXOR.js
@@ -5,3 +5,14 @@ console.log('5', (5).toString(2));
 console.log('25', (25).toString(2));
 console.log('2', (2).toString(2));
 console.log('8', (8).toString(2));
+
+
+const findMaximumXOR = (arr) => {
+    let map = new Map();
+    for (let i=0; i<arr.length; i++) {
+        map.set(arr[i], (arr[i]).toString(2));
+    }
+    console.log('map we just created', map);
+}
+
+findMaximumXOR([3, 10, 5, 25, 2, 8])
---
+++ algorithms/isRobotBounded.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+var isRobotBounded = function(instructions) {
+    // iterate instructions and use the helper function to call another function
+    // need a function to keep track of location of the robot
+    // need a function to keep directionality of robot
+    let robot = {position: [0,0], initialDirection: "N"};
+
+    
+
+    const LeftOrRight = () => {}
+
+    const GoForward = () => {
+
+    }
+
+    const helper = (instruction) => {
+        if (instruction === "L" || instruction === "R") {
+            LeftOrRight(instruction)
+        } else {
+            GoForward(instruction)
+        }
+    }
+
+    for (let i = 0; i < instructions.length; i++) {
+
+    }
+
+
+    
+
+
+    
+};
